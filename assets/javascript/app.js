4ba5b5b59429fc9b358cd64bb34d6484293aa5fe
Laurie Anderson <plipandfirt@gmail.com>
2018-10-29 13:31:12 -0400

    added a new button option - but have to click on the newly created 2x to get it to search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,96 @@
+// Initial array of gifs
+var gifs = ["Cap", "Bat", "Widow", "One-eye", "Spider", "Hill", "Winter"];
 // Event listener for all button elements
-$("button").on("click", function () {
-    // clicked button
-    var person = $(this).attr("data-person");
 
-    // Constructing a URL to search Giphy 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+function displaygifInfo() {
+    $("button").on("click", function () {
+        // clicked button
+        var person = $(this).attr("data-person");
 
-    // Performing our AJAX call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // After the data comes back from the API
-        .then(function (response) {
-            // Storing an array of results in the results variable
-            var results = response.data;
+        // Constructing a URL to search Giphy 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-            // Looping over every result item
-            for (var i = 0; i < results.length; i++) {
+        // Performing our AJAX call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After the data comes back from the API
+            .then(function (response) {
+                // Storing an array of results in the results variable
+                var results = response.data;
 
-                // Only taking action if the photo has an appropriate rating - had to set limit -some contact was just wrong
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    // Creating a div for the gif
-                    var gifDiv = $("<div>");
+                // Looping over every result item
+                for (var i = 0; i < results.length; i++) {
 
-                    // Storing the result item's rating
-                    var rating = results[i].rating;
+                    // Only taking action if the photo has an appropriate rating - had to set limit - some content was just wrong
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                        // Creating a div for the gif
+                        var gifDiv = $("<div>");
 
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + rating);
+                        // Storing the result item's rating
+                        var rating = results[i].rating;
 
-                    // Creating an image tag
-                    var personImage = $("<img>");
+                        // Creating a paragraph tag with the result item's rating
+                        var p = $("<p>").text("Rating: " + rating);
 
-                    // Giving the image tag an src attribute of a proprty pulled off the
-                    // result item
-                    personImage.attr("src", results[i].images.fixed_height.url);
+                        // Creating an image tag
+                        var personImage = $("<img>");
 
-                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                    gifDiv.append(p);
-                    gifDiv.append(personImage);
+                        // Giving the image tag an src attribute of a proprty pulled off the
+                        // result item
+                        personImage.attr("src", results[i].images.fixed_height.url);
 
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#gifs-appear-here").prepend(gifDiv);
+                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                        gifDiv.append(p);
+                        gifDiv.append(personImage);
+
+                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                        $("#gifs-appear-here").prepend(gifDiv);
+                    }
                 }
-            }
+            });
         });
+
+}
+
+// Function for displaying gif data
+function renderButtons() {
+
+    // Deleting the gifs prior to adding new gifs
+    $("#buttons-view").empty();
+
+    // Looping through the array of gifs
+    for (var i = 0; i < gifs.length; i++) {
+
+        var a = $("<button>");
+        // Adding a class of gif-btn to our button
+        a.addClass("gif-btn");
+        // Adding a data-attribute
+        a.attr("data-person", gifs[i]);
+        // Providing the initial button text
+        a.text(gifs[i]);
+        // Adding the button to the buttons-view div
+        $("#buttons-view").append(a);
+    }
+}
+
+// This function handles events where a gif button is clicked
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var gif = $("#gif-input").val().trim();
+
+    // Adding gof from the textbox to our array
+    gifs.push(gif);
+
+    // Calling renderButtons which handles the processing of our gif array
+    renderButtons();
 });
+
+// Adding a click event listener to all elements with a class of "gif-btn"
+$(document).on("click", ".gif-btn", displaygifInfo);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
 
